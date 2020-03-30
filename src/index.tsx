33fac9c1d9bc64819c54127dbd8c67119adace02
Sanjaya Sapkota <sonzayspk1998@gmail.com>
2020-03-30 16:09:57 +0545

    restructure

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "store";
 
 import "styles/css/index.css";
-import App from "./App";
+import App from "views/landingPage/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
